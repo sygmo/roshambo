16bb3931141394ab71048f68f1c0d3deddab1e8f
sygmo <sydney.mercier91@gmail.com>
2016-02-26 21:35:47 -0600

    One button added

---
+++ Roshambo/PlayViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roshambo
+//
+//  Created by Sydney Mercier on 2/26/16.
+//  Copyright © 2016 Sydney Mercier. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- Roshambo/ResponseViewController.swift
+++ Roshambo/ResponseViewController.swift
@@ -1,25 +1,25 @@
 //
-//  ViewController.swift
+//  ResponseViewController.swift
 //  Roshambo
 //
 //  Created by Sydney Mercier on 2/26/16.
 //  Copyright © 2016 Sydney Mercier. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ResponseViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
